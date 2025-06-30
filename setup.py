b5b47c0f6f1151b4d75620f03bf75f79962c051a
SrMichi19 <juan.acosta@estudiantes.utec.edu.uy>
2025-06-30 20:00:44 -0300

    README

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-# from setuptools import find_packages, setup
+from setuptools import find_packages, setup
 
-# setup(
-#     name='Trabajo Integrador Final',
-#     version='0.1.0',
-#     author='Juan Acosta, Florencia Sosa, Joaquin Sanchez',
-#     author_email='juan.acosta@estudiantes.utec.edu.uy', florencia.sosa@estudiantes.utec.edu.uy, joaquin.sanchez@estudiantes.utec.edu.uy
-#     description='Diseño, desarrollo e implementación de una librería para el procesamiento, análisis y visualización de señales fisiológicas de EEG, ECG y EMG, utilizando Programación Orientada a Objetos (POO)',
-#     python_requires='>=3.7',
-# )
+setup(
+    name='Trabajo Integrador Final',
+    version='0.1.0',
+    author='Juan Acosta, Florencia Sosa, Joaquin Sanchez',
+    author_email='juan.acosta@estudiantes.utec.edu.uy, florencia.sosa@estudiantes.utec.edu.uy, joaquin.sanchez@estudiantes.utec.edu.uy',
+    description='Diseño, desarrollo e implementación de una librería para el procesamiento, análisis y visualización de señales fisiológicas de EEG, ECG y EMG, utilizando Programación Orientada a Objetos (POO)',
+    python_requires='>=3.7'
+    )
